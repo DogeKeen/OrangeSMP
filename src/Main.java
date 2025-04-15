b4305f3977b90a6aa8205c357501fec7c91fb84f
DogeKeen <odevin482@gmail.com>
2025-04-15 08:40:58 -0500

    Update 1.1.5_alpha

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,17 @@ import java.awt.event.KeyEvent;
 
 public class Main {
     public static int keyPressed = 0;
+    public static JsonLoader jsonLoad = new JsonLoader();
 
     public static void main(String[] args) {
-        JsonLoader jsonLoad = new JsonLoader();
+        if (jsonLoad.options().debug) {
+            System.out.println("> Main.java || Program started.\n> Debug mode is turned on! Here's an explanation of the symbols:");
+            System.out.println("> > is a normal output. No need to worry about these as they are safe and state that the line works.");
+            System.out.println("> ? is a warning output. Pay a little attention to these as they state that a line may have an issue.");
+            System.out.println("> ! is a safe error output. Pay attention to these and take action if necessary as they state that there is an issue with a line that could be causing problems.");
+            System.out.println("> #! is a fatal error output. Take action with these as they state that a line is causing the program to crash.");
+            System.out.println("> If you are simply a user of this program and you encounter an error, create an issue at https://github.com/DogeKeen/OrangeSMP/issues.\n> Otherwise, if you're a developer, take the necessary actions to fix it.\n");
+        }
         Window win = new Window(jsonLoad.options().screenSize, jsonLoad.options().colourMode, new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent key) {
--- src/Window.java
+++ src/Window.java
@@ -7,44 +7,105 @@ public class Window extends JFrame {
         switch (ratio) {
             case "4:3" -> {
                 setSize(1024, 768);
+                if (Main.jsonLoad.options().debug) {
+                    System.out.println("> Window.java || Set window size to 1024x768 (4:3).");
+                }
                 setExtendedState(NORMAL);
+                if (Main.jsonLoad.options().debug) {
+                    System.out.println("> Window.java || Set extended state to normal.");
+                }
                 setLocationRelativeTo(null);
+                if (Main.jsonLoad.options().debug) {
+                    System.out.println("> Window.java || Centred the window.");
+                }
             }
             case "16:9" -> {
                 setSize(1368, 768);
+                if (Main.jsonLoad.options().debug) {
+                    System.out.println("> Window.java || Set window size to 1368x768 (16:9).");
+                }
                 setExtendedState(NORMAL);
+                if (Main.jsonLoad.options().debug) {
+                    System.out.println("> Window.java || Set extended state to normal.");
+                }
                 setLocationRelativeTo(null);
+                if (Main.jsonLoad.options().debug) {
+                    System.out.println("> Window.java || Centred the window.");
+                }
             }
             case "16:10" -> {
                 setSize(1440, 900);
+                if (Main.jsonLoad.options().debug) {
+                    System.out.println("> Window.java || Set window size to 1440x900 (16:10).");
+                }
                 setExtendedState(NORMAL);
+                if (Main.jsonLoad.options().debug) {
+                    System.out.println("> Window.java || Set extended state to normal.");
+                }
                 setLocationRelativeTo(null);
+                if (Main.jsonLoad.options().debug) {
+                    System.out.println("> Window.java || Centred the window.");
+                }
             }
             case "Maximised" -> {
                 setSize(1368, 768);
+                if (Main.jsonLoad.options().debug) {
+                    System.out.println("? Window.java || Set window size to 1368x768 (16:9).\n?? Line 50 || Window scale is redundant due to the window being maximised.");
+                }
                 setExtendedState(MAXIMIZED_BOTH);
+                if (Main.jsonLoad.options().debug) {
+                    System.out.println("> Window.java || Set extended state to maximised.");
+                }
             }
             default -> {
                 setSize(1024, 768);
+                if (Main.jsonLoad.options().debug) {
+                    System.out.println("! Window.java || Set window size to 1024x768 (4:3).");
+                }
                 setExtendedState(NORMAL);
+                if (Main.jsonLoad.options().debug) {
+                    System.out.println("! Window.java || Set extended state to normal.");
+                }
                 setLocationRelativeTo(null);
-                System.out.println("\"screenSize\" variable is invalid.");
+                if (Main.jsonLoad.options().debug) {
+                    System.out.println("! Window.java || Centred the window.\n!! Line 7 || The value for \"screenSize\" is invalid. The window has loaded with its default parameters.");
+                }
             }
         }
 
         if (colour.equals("Dark")) {
             getContentPane().setBackground(Color.BLACK);
+            if (Main.jsonLoad.options().debug) {
+                System.out.println("> Window.java || Set background colour to black.");
+            }
         } else if (colour.equals("Light")) {
             getContentPane().setBackground(Color.WHITE);
+            if (Main.jsonLoad.options().debug) {
+                System.out.println("> Window.java || Set background colour to white.");
+            }
         } else {
             getContentPane().setBackground(Color.BLACK);
-            System.out.println("\"colourMode\" variable is invalid.");
+            if (Main.jsonLoad.options().debug) {
+                System.out.println("! Window.java || Set background colour to black.\n!! Line 76 || The value for \"colourMode\" is invalid. The background has been set to its default parameter.");
+            }
         }
 
         setResizable(false);
+        if (Main.jsonLoad.options().debug) {
+            System.out.println("> Window.java || Disabled resizing.");
+        }
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        if (Main.jsonLoad.options().debug) {
+            System.out.println("> Window.java || Program stops when the window is closed.");
+        }
         setVisible(true);
+        if (Main.jsonLoad.options().debug) {
+            System.out.println("> Window.java || Window is visible.");
+        }
 
         addKeyListener(listener);
+        if (Main.jsonLoad.options().debug) {
+            System.out.println("> Window.java || Listening to key inputs.");
+        }
     }
 }
